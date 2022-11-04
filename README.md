2713c13f4d9ea34a47647a9a742179a51145e7a1
S556508 <112007811+S556508@users.noreply.github.com>
2022-11-03 22:30:22 -0500

    added all files in new directory

---
+++ README.md
@@ -0,0 +1,5 @@
+# f2wb08meesala
+https://f2wb08meesala.onrender.com
+
+# ClassName:House
+# Attributes:house_Name, house_Type,house_Cost
---
+++ routes/house.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('house', { title: 'Search Results for house' });
+  
+});
+
+module.exports = router;
