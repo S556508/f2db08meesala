dd1b3f717596d1594533c0a49716dd8319f2401e
S556508 <112007811+S556508@users.noreply.github.com>
2022-11-16 15:34:02 -0600

    added housedetails.pug file

--- controllers/house.js
+++ controllers/house.js
@@ -28,11 +28,33 @@ exports.house_create_post = async function(req, res) {
     }   
 }; 
 
- 
-// Handle house delete form on DELETE. 
-exports.house_delete = function(req, res) { 
-    res.send('NOT IMPLEMENTED: house delete DELETE ' + req.params.id); 
-}; 
+// Handle a show one view with id specified by query
+exports.house_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await house.findById( req.query.id)
+    res.render('housedetail',
+    { title: 'house Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+
+
+// Handle Costume delete on DELETE.
+exports.house_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await house.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+    };
  
 // Handle house update form on PUT. 
 exports.house_update_put = function(req, res) { 
