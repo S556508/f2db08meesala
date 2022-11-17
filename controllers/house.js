30d11b54e2ebda5bccbb26bc73cadce0308ea5cb
S556508 <112007811+S556508@users.noreply.github.com>
2022-11-16 20:33:55 -0600

    made the changes

--- controllers/house.js
+++ controllers/house.js
@@ -115,3 +115,57 @@ exports.house_detail = async function(req, res) {
     failed`);
     }
     };
+
+    // Handle a show one view with id specified by query
+exports.house_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await house.findById( req.query.id)
+    res.render('housedetail',
+    { title: 'house Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+// Handle building the view for creating a house.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.house_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('housecreate', { title: 'house Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+// Handle building the view for updating a house.
+// query provides the id
+exports.house_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await house.findById(req.query.id)
+    res.render('houseupdate', { title: 'house Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+// Handle a delete one view with id from query
+exports.house_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await house.findById(req.query.id)
+    res.render('housedelete', { title: 'house Delete', toShow:
+    result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+     
--- routes/house.js
+++ routes/house.js
@@ -7,4 +7,26 @@ router.get('/', function(req, res, next) {
   
 });
 
+/* GET home page. */
+
+router.get('/', function(req, res, next) {
+
+  res.render('house', { title: 'Search Results house' });
+
+});
+
+var express = require('express');
+
+const house_controllers= require('../controllers/house');
+
+var router = express.Router();
+
+/* GET detail house page */
+router.get('/detail', house_controllers.house_view_one_Page);
+/* GET create costume page */
+router.get('/create', house_controllers.house_create_Page);
+/* GET create update page */
+router.get('/update', house_controllers.house_update_Page);
+/* GET delete house page */
+router.get('/delete', house_controllers.house_delete_Page);
 module.exports = router;
